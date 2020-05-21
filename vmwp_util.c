bc5b392f715dd4cf2fb922c7f46a578665fb07c7
Meysam Firouzi <meysam@hatching.io>
2020-05-21 22:26:43 +0200

    style

--- vmwp_util.c
+++ vmwp_util.c
@@ -1,22 +1,19 @@
 #include "pch.h"
 #include "tdriver.h"
 
-
-
 BOOLEAN TdProcessNotifyRoutineSet2 = FALSE;
-DRIVER_INITIALIZE  DriverEntry;
+DRIVER_INITIALIZE DriverEntry;
 
 _Dispatch_type_(IRP_MJ_CREATE) DRIVER_DISPATCH TdDeviceCreate;
 
-DRIVER_UNLOAD   TdDeviceUnload;
+DRIVER_UNLOAD TdDeviceUnload;
 
-typedef struct _OSR_WORK_ITEM 
+typedef struct _OSR_WORK_ITEM
 {
-    WORK_QUEUE_ITEM    WorkItem;
-    PVOID              Process;
+    WORK_QUEUE_ITEM WorkItem;
+    PVOID Process;
 
-} OSR_WORK_ITEM, * POSR_WORK_ITEM;
-
+} OSR_WORK_ITEM, *POSR_WORK_ITEM;
 
 int MitigationFlags = 0;
 char SignatureLevel = 0;
@@ -30,33 +27,27 @@ VOID WorkRoutine(PVOID Parameter)
     LARGE_INTEGER delay;
 
     delay.QuadPart = -10000 * 2000;
-//    KeDelayExecutionThread(KernelMode, FALSE, &delay);
-
+    //    KeDelayExecutionThread(KernelMode, FALSE, &delay);
 
     if (STATUS_SUCCESS == KeDelayExecutionThread(KernelMode, FALSE, &delay))
     {
         vmwpProcess = OsrWorkItem->Process;
-		
-        memset(((char*)OsrWorkItem->Process) + 0x850, 0, sizeof(int));  // +0x850 MitigationFlags  : Uint4B
-        memset(((char*)OsrWorkItem->Process) + 0x6f8, 0, sizeof(char));  // +0x6f8 SignatureLevel   : UChar
-        memset(((char*)OsrWorkItem->Process) + 0x6f9, 0, sizeof(char));  //  + 0x6f9 SectionSignatureLevel : UChar
+
+        memset(((char *)OsrWorkItem->Process) + 0x850, 0, sizeof(int));  // +0x850 MitigationFlags  : Uint4B
+        memset(((char *)OsrWorkItem->Process) + 0x6f8, 0, sizeof(char)); // +0x6f8 SignatureLevel   : UChar
+        memset(((char *)OsrWorkItem->Process) + 0x6f9, 0, sizeof(char)); //  + 0x6f9 SectionSignatureLevel : UChar
     }
 
     ExFreePool(OsrWorkItem);
 }
 
-
-
-VOID
-TdCreateProcessNotifyRoutine2 (
+VOID TdCreateProcessNotifyRoutine2(
     _Inout_ PEPROCESS Process,
     _In_ HANDLE ProcessId,
-    _In_opt_ PPS_CREATE_NOTIFY_INFO CreateInfo
-    )
+    _In_opt_ PPS_CREATE_NOTIFY_INFO CreateInfo)
 {
     UNREFERENCED_PARAMETER(ProcessId);
 
-   
     if (CreateInfo != NULL)
     {
         UNICODE_STRING vmwp = RTL_CONSTANT_STRING(L"\\??\\C:\\Windows\\System32\\vmwp.exe");
@@ -67,29 +58,25 @@ TdCreateProcessNotifyRoutine2 (
             OsrWorkItem->Process = (PVOID)Process;
             ExInitializeWorkItem(&OsrWorkItem->WorkItem, WorkRoutine, OsrWorkItem);
             ExQueueWorkItem(&OsrWorkItem->WorkItem, DelayedWorkQueue);
-
         }
     }
-
 }
 
-
 NTSTATUS
-DriverEntry (
-    _In_ PDRIVER_OBJECT  DriverObject,
-    _In_ PUNICODE_STRING RegistryPath
-)
+DriverEntry(
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath)
 {
     NTSTATUS Status;
-    UNREFERENCED_PARAMETER (RegistryPath);
+    UNREFERENCED_PARAMETER(RegistryPath);
 
-    DriverObject->MajorFunction[IRP_MJ_CREATE]         = TdDeviceCreate;
-    DriverObject->MajorFunction[IRP_MJ_CLOSE]          = TdDeviceCreate;
-    DriverObject->MajorFunction[IRP_MJ_CLEANUP]        = TdDeviceCreate;
+    DriverObject->MajorFunction[IRP_MJ_CREATE] = TdDeviceCreate;
+    DriverObject->MajorFunction[IRP_MJ_CLOSE] = TdDeviceCreate;
+    DriverObject->MajorFunction[IRP_MJ_CLEANUP] = TdDeviceCreate;
     DriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = TdDeviceCreate;
-    DriverObject->DriverUnload                         = TdDeviceUnload;
-    
-    Status = PsSetCreateProcessNotifyRoutineEx (TdCreateProcessNotifyRoutine2,FALSE );
+    DriverObject->DriverUnload = TdDeviceUnload;
+
+    Status = PsSetCreateProcessNotifyRoutineEx(TdCreateProcessNotifyRoutine2, FALSE);
 
     if (!NT_SUCCESS(Status))
     {
@@ -102,10 +89,8 @@ DriverEntry (
     return Status;
 }
 
-VOID
-TdDeviceUnload (
-    _In_ PDRIVER_OBJECT DriverObject
-)
+VOID TdDeviceUnload(
+    _In_ PDRIVER_OBJECT DriverObject)
 {
     NTSTATUS Status = STATUS_SUCCESS;
 
@@ -113,24 +98,20 @@ TdDeviceUnload (
 
     if (TdProcessNotifyRoutineSet2 == TRUE)
     {
-        Status = PsSetCreateProcessNotifyRoutineEx (TdCreateProcessNotifyRoutine2, TRUE );
+        Status = PsSetCreateProcessNotifyRoutineEx(TdCreateProcessNotifyRoutine2, TRUE);
     }
-
 }
 
-
 NTSTATUS
-TdDeviceCreate (
-    IN PDEVICE_OBJECT  DeviceObject,
-    IN PIRP  Irp
-)
+TdDeviceCreate(
+    IN PDEVICE_OBJECT DeviceObject,
+    IN PIRP Irp)
 {
-    UNREFERENCED_PARAMETER (DeviceObject);
+    UNREFERENCED_PARAMETER(DeviceObject);
 
     Irp->IoStatus.Status = STATUS_SUCCESS;
     Irp->IoStatus.Information = 0;
-    IoCompleteRequest (Irp, IO_NO_INCREMENT);
+    IoCompleteRequest(Irp, IO_NO_INCREMENT);
 
     return STATUS_SUCCESS;
 }
-
